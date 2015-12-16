4e5d7af64ed0cdc8984c03cde7055085ea6199bb
Muammar El Khatib <muammar@debian.org>
2015-12-16 14:15:38 +0100

    Back to nm-applet for the moments.

--- rc.lua
+++ rc.lua
@@ -547,7 +547,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 --os.execute("gnome-sound-applet &") --- deprecated
 os.execute("gnome-terminal &")
 os.execute("/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &")
---os.execute("nm-applet &")
+os.execute("nm-applet &")
 -- End GNome
 -- GB Keyboard
 --os.execute("setxkbmap -model pc105 -layout gb -variant mac_intl -option grp:shift_caps_toggle &")
