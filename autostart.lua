9a28aa82e66661fab9e7a456546b75a06ebfb178
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-08-13 11:49:38 +0200

    Some more love to autostart.lua.

--- autostart.lua
+++ autostart.lua
@@ -1,38 +1,45 @@
 -- Startup programs
 --
-
 local awful = require("awful")
--- Starting before spawn_with_shell
+
+-- Programs starting before spawn_with_shell
 --
 os.execute("gnome-terminal &")
 os.execute("wicd-client -t &")
 --testing os.execute("/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &")
 --os.execute("nm-applet &")
 
--- GB Keyboard
---os.execute("setxkbmap -model pc105 -layout gb -variant mac_intl -option grp:shift_caps_toggle &")
+-- Keyboard configuration
+--
 -- US keyboard
 os.execute("setxkbmap -model pc105 -layout us -variant intl -option grp:shift_caps_toggle &")
----os.execute("xrdb -merge ~/.Xresources")
---
+-- GB Keyboard
+--os.execute("setxkbmap -model pc105 -layout gb -variant mac_intl -option grp:shift_caps_toggle &")
+
 -- GNome components
---awful.util.spawn_with_shell("gnome-session")
---awful.util.spawn_with_shell("gnome-screensaver")
 awful.util.spawn_with_shell("gnome-settings-daemon")
 --testing awful.util.spawn_with_shell("gnome-keyring-daemon")
--- End GNome
---
 
---awful.util.spawn_with_shell("sh ~/.config/awesome/scripts/monitorretina")
+-- Power management
 awful.util.spawn_with_shell("xfce4-power-manager")
-awful.util.spawn_with_shell("sleep 20; volumeicon") -- Volume icon
-awful.util.spawn_with_shell("rm ~/.dispad.pid; dispad -F")
-awful.util.spawn_with_shell("sleep 5; conky -c /home/$USER/.config/awesome/conky/conky_simple/conkyrc")
-awful.util.spawn_with_shell("dropbox start")
-awful.util.spawn_with_shell("/home/$USER/.config/awesome/scripts/xmodmapawesome")
---awful.util.spawn_with_shell("xscreensaver -no-splash")
-awful.util.spawn_with_shell("feh --bg-fill /home/$USER/.config/awesome/wallpaper/funnysystem.jpg") --Set wallpaper
 
--- HiDPI specifics
---
+-- Volume icon
+awful.util.spawn_with_shell("sleep 20; volumeicon")
+
+-- Deactivate keyboard while typing
+awful.util.spawn_with_shell("rm ~/.dispad.pid; dispad -F")
+
+-- Start Conky
+awful.util.spawn_with_shell("sleep 5; conky -c /home/$USER/.config/awesome/conky/conky_simple/conkyrc")
+
+-- Start Dropbox Daemon
+awful.util.spawn_with_shell("dropbox start")
+
+-- Set wallpaper and screensaver
+awful.util.spawn_with_shell("feh --bg-fill /home/$USER/.config/awesome/wallpaper/funnysystem.jpg")
+--awful.util.spawn_with_shell("xscreensaver -no-splash")
+
+-- Configuration scripts and HiDPI specifics
+awful.util.spawn_with_shell("/home/$USER/.config/awesome/scripts/xmodmapawesome")
 awful.util.spawn_with_shell("xrdb -merge /home/$USER/.config/awesome/scripts/Xdefaults.hidpi")
+--awful.util.spawn_with_shell("sh ~/.config/awesome/scripts/monitorretina")
